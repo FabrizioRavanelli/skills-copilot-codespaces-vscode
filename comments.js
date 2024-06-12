053d1b9ba81e037410d14b16dce6859cb80b2f69
Sigfrid Couto Fernández <sigfridcouto@gmail.com>
2024-06-12 17:54:51 +0000

    Copilot third commit - 2

--- comments.js
+++ comments.js
@@ -1,47 +1,8 @@
 // create web server
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
+const port = 3000;
 const fs = require('fs');
+const path = require('path');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// get all comments
-app.get('/comments', (req, res) => {
-    fs.readFile(__dirname + '/comments.json', 'utf8', (err, data) => {
-        if (err) {
-            res.status(500).send('Error reading comments.json');
-            return;
-        }
-
-        res.setHeader('Content-Type', 'application/json');
-        res.send(data);
-    });
-});
-
-// add a comment
-app.post('/comments', (req, res) => {
-    fs.readFile(__dirname + '/comments.json', 'utf8', (err, data) => {
-        if (err) {
-            res.status(500).send('Error reading comments.json');
-            return;
-        }
-
-        const comments = JSON.parse(data);
-        comments.push(req.body);
-
-        fs.writeFile(__dirname + '/comments.json', JSON.stringify(comments), 'utf8', (err) => {
-            if (err) {
-                res.status(500).send('Error writing comments.json');
-                return;
-            }
-
-            res.status(201).send('Comment added');
-        });
-    });
-});
-
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
-});
+app.use(express.static('public'));
