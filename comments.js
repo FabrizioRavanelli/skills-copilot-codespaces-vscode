a8df2345e5b3327496f44130367771eb97a9a91a
Sigfrid Couto Fernández <sigfridcouto@gmail.com>
2024-06-12 18:02:14 +0000

    Copilot third commit

--- comments.js
+++
@@ -1,8 +0,0 @@
-// create web server
-const express = require('express');
-const app = express();
-const port = 3000;
-const fs = require('fs');
-const path = require('path');
-
-app.use(express.static('public'));
